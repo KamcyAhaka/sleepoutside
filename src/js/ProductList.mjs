2ba87b1dad1d8552a8eba63bd13d69351b0aed42
KamCy Ahaka <kamcyahaka@gmail.com>
2024-10-12 14:39:47 +0100

    feat: add discount indicator

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -10,7 +10,10 @@ function productCardTemplate(product) {
       />
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.Name}</h2>
-      <p class="product-card__price">$${product.FinalPrice}</p>
+      <div class="price-container">
+        <p class="product-card__price">$${product.FinalPrice}</p>
+        <p class="product-card__suggestedRetailPrice">$${product.SuggestedRetailPrice}</p>
+      </div>
     </a>
   </li>
   `
